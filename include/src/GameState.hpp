157019fbd3f4f8c80d63f25dd4f62ce1a16d297f
NaCryptoKe <Nahomcryptoketema@gmail.com>
2026-08-26 14:54:27 +0300

    feat: add GameState for everything of the game

---
+++ include/src/GameState.hpp
@@ -0,0 +1,21 @@
+#ifndef GAME_STATE_HPP
+#define GAME_STATE_HPP
+
+enum class GameState
+{
+    MAIN_MENU,
+
+    CHARACTER_SELECT,
+    
+    FIGHTING,
+    PAUSED,
+
+    ROUND_OVER,
+    MATCH_OVER,
+
+    SETTINGS,
+    
+    GAME_OVER
+};
+
+#endif // GAME_STATE_HPP
